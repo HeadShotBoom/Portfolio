db5f6dd6b87c5b026d039450b922796c0681646f
Clayton Allen <clayton.allen.us@gmail.com>
2015-09-09 19:22:30 -0500

    Adding some blade structure

---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	{{-- Include the page meta --}}
+	@include('layout.meta')
+
+	{{-- Helper to generate SEO friendly page titles --}}
+	<title>Legendary Productions - @section('Welcome')</title>
+
+	{{-- Include CSS file and such --}}
+	@include('layout.links')
+	
+</head>
+<body>
+
+	{{-- Include Nav --}}
+	@include('layout.navbar')
+
+	@yield('content')
+
+	{{-- Include Scripts --}}
+	@include('layout.scripts')
+</body>
+</html>
